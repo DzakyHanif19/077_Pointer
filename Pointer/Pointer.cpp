6edeb752eab445d7d8f4301fa290ece420b2eb83
DzakyHanif19 <dzakyannafi1@gmail.com>
2023-05-10 10:54:00 +0700

    class mahasiswa

--- Pointer/Pointer.cpp
+++ Pointer/Pointer.cpp
@@ -1,16 +1,3 @@
 #include <iostream>
 using namespace std;
-class mahasiswa {
-public:
-    int main;
-    void showNim();
-};
-
-void mahasiswa::showNim(); {
-    cout << "No Induk = " << nim << endl;
-}
-int main()
-{
-    mahasiswa mhs(1);
-    mhs.showNim();
-}
+class mahasiswa
